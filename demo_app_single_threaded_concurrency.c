e7052ada7f2493460b5225159ba935891b1f7b89
sachinites <sachinites@gmail.com>
2021-12-19 00:00:04 -0800

    IInd Demo which shows single threaded Concurreny Added.

---
+++ demo_app_single_threaded_concurrency.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "event_loop.h"
+
+event_loop_t el;
+static int upload = 0;
+static int download = 0;
+
+static void
+upload_fn(void *arg) {
+
+    while(upload < 100) {
+    upload += 2;
+    printf ("upload percent-age = %d\n", upload);
+    if (upload % 10 == 0 && upload != 100) {
+        task_create_new_job(&el, upload_fn, NULL);
+        return;
+    }
+    }
+}
+
+static void
+download_fn(void *arg) {
+
+    while(download < 100) {
+    download += 2;
+    printf ("download percent-age = %d\n", download);
+    if (download % 10 == 0 && download != 100) {
+        task_create_new_job(&el, download_fn, NULL);
+        return;
+    }
+    }
+}
+
+
+int
+main(int argc, char **argv) {
+
+    event_loop_init(&el);
+    event_loop_run(&el);
+    sleep(1);
+
+    task_create_new_job(&el, upload_fn, NULL);
+    task_create_new_job(&el, download_fn, NULL);
+    
+    printf ("End of main\n");
+    scanf("\n");
+    return 0;
+}
