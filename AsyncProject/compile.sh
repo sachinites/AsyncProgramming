884128b64309d89b4d5626d52ebc9598be1a6b7c
sachinites <sachinites@gmail.com>
2022-01-22 03:41:28 -0800

    Assignment file Renaming

--- AsyncProject/compile.sh
+++ AsyncProject/compile.sh
@@ -1,7 +1,9 @@
+rm *o
+rm *exe
 gcc -g -c network_utils.c -o network_utils.o
-gcc -g -c rtm.c -o rtm.o
+gcc -g -c stp.c -o stp.o
 gcc -g -c utils.c -o utils.o
 gcc -g -c udp_client.c -o udp_client.o
 gcc -g -c rt.c -o rt.o
-gcc -g rtm.o utils.o rt.o network_utils.o -o exe -lpthread
+gcc -g stp.o utils.o rt.o network_utils.o -o stp.exe -lpthread
 gcc -g udp_client.o network_utils.o utils.o rt.o -o udp_client.exe -lpthread
